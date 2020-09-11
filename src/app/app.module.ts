387102dfa0e671e8e5baa36c2ef90ec52b57fc04
Gevorg1990 <67325643+Gevorg1990@users.noreply.github.com>
2020-09-11 21:09:33 +0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FilmComponent } from './components/film/film.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilmComponent
   ],
   imports: [
     BrowserModule,
